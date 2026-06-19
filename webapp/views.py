2d414f84bb1818845911a7872127eb4167f56570
Uric01 <uric01@live.com>
2026-06-19 21:24:58 +0200

    html pages mapping

--- webapp/views.py
+++ webapp/views.py
@@ -2,20 +2,20 @@ from django.http import Http404
 from django.shortcuts import render
 
 ALLOWED_PAGES = {
-    'dashboard': 'dashboard.html',
-    'assets': 'assets.html',
-    'risks': 'risks.html',
-    'add_asset': 'add_asset.html',
-    'add_risk': 'add_risk.html',
-    'add_mitigation': 'add_mitigation.html',
-    'reports': 'reports.html',
-    'audit_logs': 'audit_logs.html',
-    'login': 'login.html',
+    'dashboard': 'risk_whisperers/dashboard.html',
+    'assets': 'risk_whisperers/assets.html',
+    'risks': 'risk_whisperers/risks.html',
+    'add_asset': 'risk_whisperers/add_asset.html',
+    'add_risk': 'risk_whisperers/add_risk.html',
+    'add_mitigation': 'risk_whisperers/add_mitigation.html',
+    'reports': 'risk_whisperers/reports.html',
+    'audit_logs': 'risk_whisperers/audit_logs.html',
+    'login': 'risk_whisperers/login.html',
 }
 
 
 def home(request):
-    return render(request, 'login.html')
+    return render(request, ALLOWED_PAGES['login'])
 
 
 def page(request, page_name):
