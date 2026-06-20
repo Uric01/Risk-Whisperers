eb633aa4646e50de1a33d93923af718592c02613
Uric01 <uric01@live.com>
2026-06-20 16:27:33 +0200

    Basic user logn created

--- users/views.py
+++ users/views.py
@@ -6,10 +6,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.models import Group, User
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 
 def index(request):
-     return render(request,"users/login.html")
+     return render(request,"risk_whisperers/login.html")
     
 
 def login_view(request):
@@ -17,27 +18,19 @@ def login_view(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        return redirect('dashboard')  # Redirect to the dashboard page after successful login
+        
+          # Redirect to the dashboard page after successful login
     
-       # if user is not None:
-        #    login(request, user)
-            
-            # Route to appropriate dashboard based on group
-            #if user.groups.filter(name='Admin').exists() or user.is_staff:
-            #    return render(request, 'users/dashboard.html')
-            #elif user.groups.filter(name='Auditor').exists():
-            #    return render(request, 'users/audit_logs.html')
-            #elif user.groups.filter(name='Risk_Manager').exists():
-            #    return render(request, 'risk_whisperers/add_risk.html')
-            #
-            # Default redirect
-            
-        #else:
-       #     return render(request, 'users/login.html', {"error": "Invalid username or password"})
-    
-    #return render(request, 'users/login.html')
+        if user is not None:
+           login(request, user)
+           messages.success(request, "Login successful.")
+           return render(request, 'risk_whisperers/dashboard.html')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return render(request, 'risk_whisperers/login.html')
+    return render(request, 'risk_whisperers/login.html')
 
 
 def logout_view(request):
     logout(request)
-    return redirect("login")
+    return render(request, 'risk_whisperers/login.html')
